727d7fb85bd3409a985e90544f300e540d921aae
Apex Dada <kaziwoaej@gmail.com>
2026-08-16 04:17:36 +0600

    Wrap SocialGridWidget with standard BentoCard container

--- lib/features/home/widgets/social_grid_widget.dart
+++ lib/features/home/widgets/social_grid_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:portfolio/features/home/provider/home_viewmodel.dart';
+import 'package:portfolio/core/widgets/bento_card.dart';
 
 class SocialGridWidget extends StatelessWidget {
   const SocialGridWidget({super.key});
@@ -9,6 +10,10 @@ class SocialGridWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDark = context.select<HomeViewModel, bool>((vm) => vm.isDarkMode);
+    final cardColor = context.select<HomeViewModel, Color>((vm) => vm.cardColor);
+    final borderColor = context.select<HomeViewModel, Color>((vm) => vm.borderColor);
+    final hoverBorderColor = context.select<HomeViewModel, Color>((vm) => vm.hoverBorderColor);
+
     final iconColor = isDark ? Colors.white.withValues(alpha: 0.85) : Colors.black87;
 
     final socialItems = [
@@ -59,8 +64,11 @@ class SocialGridWidget extends StatelessWidget {
       },
     ];
 
-    return Container(
-      color: Colors.transparent,
+    return BentoCard(
+      backgroundColor: cardColor,
+      borderColor: borderColor,
+      hoverBorderColor: hoverBorderColor,
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           Expanded(
