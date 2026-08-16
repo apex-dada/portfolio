b100d7a6738b9c50cf15d20867313f38ce5218b3
Apex Dada <kaziwoaej@gmail.com>
2026-08-17 02:51:45 +0600

    Pin Flutter version to match local working environment

--- vercel-build.sh
+++ vercel-build.sh
@@ -3,7 +3,10 @@ set -e
 
 echo "=== Installing Flutter SDK ==="
 if [ ! -d "flutter" ]; then
-  git clone https://github.com/flutter/flutter.git --depth 1 -b stable flutter
+  git clone https://github.com/flutter/flutter.git flutter
+  cd flutter
+  git checkout 00b0c91f06209d9e4a41f71b7a512d6eb3b9c694
+  cd ..
 fi
 
 export PATH="$PATH:$PWD/flutter/bin"
